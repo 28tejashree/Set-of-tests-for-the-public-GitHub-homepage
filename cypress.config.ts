3507084dbc7ce3463d9a1225e3653b969d5a156f
28tejashree <tejashree28.work@gmail.com>
2024-10-30 21:53:40 +0530

    update yml file

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  projectId: '7gh68i',
   screenshotOnRunFailure: true,
   screenshotsFolder: 'cypress/reports/screenshots',
   video: true,
